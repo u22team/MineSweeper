afe793c8ef518dbd1755fd6fe2c2120fe8e1ce33
Aty Nagano <atynagano@gmail.com>
2019-10-16 18:44:20 +0900

    Map生成まで

--- Assets/MineManager.cs
+++ Assets/MineManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MineManager : MonoBehaviour
 {
-    bool[,] mines;
-    int[,] mineCount;
+    //bool[,] mines;
+    mapStat map;
     Vector2Int mapSize;
 
     void Start()
@@ -17,38 +17,79 @@ public class MineManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            mines = CreateMap(mapSize, MousePosToMap(Input.mousePosition));
-            mineCount = CreateMineCounts(mines);
+            if (map == null)
+                map = CreateMap(mapSize, MousePosToMap(Input.mousePosition));
+            else
+            {
+
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+
         }
     }
 
-    bool[,] CreateMap(Vector2Int _mapSize, Vector2Int _mousePos)
+    mapStat CreateMap(Vector2Int _mapSize, Vector2Int _mousePos)
     {
-        bool[,] stage;
-        stage = new bool[_mapSize.x, _mapSize.y];
+        mapStat _map = new mapStat();
+        _map.squareStats = new squareStat[_mapSize.x, _mapSize.y];
+        _map = InstallMines(_map, _mousePos);
+        _map = CountMines(_map);
+        return _map;
+    }
+
+    mapStat InstallMines(mapStat _map, Vector2Int _mousePos)
+    {
+        Vector2Int _mapSize = GetMapSize(_map.squareStats);
         for (int i = 0; i < _mapSize.x * _mapSize.y / 10; i++)
         {
-            Vector2Int _mapPos = new Vector2Int(); //初期化必要？
-            bool isfilled = true;
-            while (isfilled)
+            Vector2Int _minePos = new Vector2Int(); //初期化必要？
+            bool cannotInstall = true;
+            while (cannotInstall)
             {
-                _mapPos.x = (int)Random.Range(0, (float)_mapSize.x + 0.99f);
-                _mapPos.y = (int)Random.Range(0, (float)_mapSize.y + 0.99f);
-                isfilled = stage[_mapPos.x, _mapPos.y] || _mapPos == _mousePos;
+                _minePos.x = (int)Random.Range(0, (float)_mapSize.x + 0.99f);
+                _minePos.y = (int)Random.Range(0, (float)_mapSize.y + 0.99f);
+                cannotInstall = _map.squareStats[_minePos.x, _minePos.y].mineStat.hasMine || IsAroundMousePos(_minePos, _mousePos, _map.squareStats);
             }
-            stage[_mapPos.x, _mapPos.y] = true;
+            _map.squareStats[_minePos.x, _minePos.y].mineStat.hasMine = true;
+            _map.minesPos.Add(new Vector2Int(_minePos.x, _minePos.y));
         }
-        return stage;
+        return _map;
     }
 
-    int[,] CreateMineCounts(bool[,] _mines)
+    bool IsAroundMousePos(Vector2Int _minePos, Vector2Int _mousePos, squareStat[,] _map)
     {
-        Vector2Int _mapSize = new Vector2Int(_mines.GetLength(0), _mines.GetLength(1));
-        int[,] _mineCount = new int[_mapSize.x, _mapSize.y];
-        for (int i = 0; i < _mapSize.x; i++)
-            for (int j = 0; j < _mapSize.y; j++)
-                GetMineCount(new Vector2Int(i, j), _mines);
-        return _mineCount;
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++)
+            {
+                Vector2Int _pos = _mousePos + new Vector2Int(i, j);
+                if (IsInMap(_pos, _map)) if (_pos == _minePos) return true;
+            }
+        return false;
+    }
+
+    mapStat CountMines(mapStat _map)
+    {
+        Vector2Int _mapSize = GetMapSize(_map.squareStats);
+
+        foreach (Vector2Int _minePos in _map.minesPos)
+        {
+            for (int i = -1; i <= 1; i++)
+                for (int j = -1; j <= 1; j++)
+                {
+                    Vector2Int _pos = _minePos + new Vector2Int(i, j);
+                    if (IsInMap(_pos, _map.squareStats)) if (_pos != _minePos) _map.squareStats[_minePos.x, _minePos.y].mineStat.mineCount += 1;
+                }
+        }
+        return _map;
+    }
+
+    //細かいメソッド
+
+    Vector2Int GetMapSize(squareStat[,] _map) //名前を修正しろ
+    {
+        return new Vector2Int(_map.GetLength(0), _map.GetLength(1));
     }
 
     Vector2Int MousePosToMap(Vector3 _mousePos) //作りかけ screenを利用してクリック先を取得
@@ -57,18 +98,30 @@ public class MineManager : MonoBehaviour
         return mapPos;
     }
 
-    int GetMineCount(Vector2Int _squarePos, bool[,] _mines)
+    bool IsInMap(Vector2Int _pos, squareStat[,] _map) //名前を修正しろ
     {
-        Vector2Int _mapSize = new Vector2Int(_mines.GetLength(0), _mines.GetLength(1));
-        int[,] around = new int[8, 2] { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
-        int _minecount = 0;
-        for (int i = 0; i < 8; i++)
-        {
-            Vector2Int _checkPos = new Vector2Int(_squarePos.x + around[i, 0], _squarePos.x + around[i, 1]);
-            if (0 <= _checkPos.x && _checkPos.x <= _mapSize.x && 0 <= _checkPos.y && _checkPos.y <= _mapSize.y) _minecount += _mines[_checkPos.x, _checkPos.y] ? 1 : 0;
-        }
-        return _minecount;
+        Vector2Int _mapSize = GetMapSize(_map);
+        return 0 <= _pos.x && _pos.x <= _mapSize.x && 0 <= _pos.y && _pos.y <= _mapSize.y;
     }
 
+    //型の宣言
 
+    class squareStat
+    {
+        bool open;
+        public class mineStatType
+        {
+            public int mineCount;
+            public bool hasMine;
+        }
+
+        public mineStatType mineStat;
+
+    }
+
+    class mapStat //地雷の位置を記憶しておいたほうが高速なのは間違いない
+    {
+        public squareStat[,] squareStats;
+        public List<Vector2Int> minesPos = new List<Vector2Int>();
+    }
 }
